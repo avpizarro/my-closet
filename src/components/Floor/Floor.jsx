f64b846bd4ad6c8e30c32ccb37db87e877ae84b1
Alejandra Valdes Pizarro <avaldespizarro@outlook.com>
2023-11-26 17:50:21 +1100

    Amend favicon for Chrome Desktop, create Threejs scenes

---
+++ src/components/Floor/Floor.jsx
@@ -0,0 +1,9 @@
+function Floor(props) {
+  return (
+    <mesh {...props} receiveShadow>
+      <boxBufferGeometry args={[20,1,10]} />
+      <meshPhysicalMaterial color='white' />
+    </mesh>
+  )
+}
+export default Floor
--- src/components/Outfit/Outfit.jsx
+++ src/components/Outfit/Outfit.jsx
@@ -1,9 +1,23 @@
+'use client';
+import { Canvas } from '@react-three/fiber';
 import styles from './Outfit.module.css';
 
 function Outfit() {
-  return (
+return (
 
-    <div className={styles.container}>Outfit</div>
-  )
+  <div className={styles.container}>
+    <div className={styles.scene}>
+      <Canvas
+      shadows
+      className={styles.canvas}
+      camera={{
+        position: [-6, 7, 7],
+      }}
+      >
+
+      </Canvas>
+    </div>
+  </div>
+)
 }
 export default Outfit
